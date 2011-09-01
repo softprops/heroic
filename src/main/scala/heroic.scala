4f71298daddb0a76be6609f5b1f8a953d004b9f0
softprops <d.tangren@gmail.com>
2011-09-01 02:17:11 -0400

    got simple app /w deps up and running

--- src/main/scala/heroic.scala
+++ src/main/scala/heroic.scala
@@ -7,7 +7,7 @@ import Project.Initialize
 object Procfile {
   def apply(tasks: Seq[(String, String)]) =
    (tasks.map {
-     case (task, cmd) => "%s : %s" format(task, cmd)
+     case (task, cmd) => "%s: %s" format(task, cmd)
    }).mkString("\n")
 }
 
@@ -15,56 +15,101 @@ object Script {
   def apply(main: String, cp: Seq[String], jvmOpts: Option[String] = None) =
   """#!/bin/sh
   |
-  |function log() {
+  |log (){
   |  COLOR="\033[0;35m"
   |  CLEAR="\033[0m"
   |  echo "$COLOR $1 $CLEAR"
   |}
   |
   |log "Building application"
-  |
   |mvn scala:compile
+  |
+  |log "Installing application"
+  |mvn install -DskipTests=true
   |
   |JAVA=`which java`
   |
   |CLASSPATH=%s
   |
-  |log "Booting application"
-  |
-  |exec $JAVA %s -classpath "$CLASSPATH" %s "$@"
+  |log "Booting application (%s)"
+  |exec $JAVA -Xmx256m -Xss2048k %s -classpath "$CLASSPATH" %s "$@"
   |""".stripMargin
       .format(
         cp.map(""""$REPO"/%s""" format _).mkString(":"),
+        main,
         jvmOpts.getOrElse(""),
         main
       )
 }
 
-/*object Foreman {
+case class Gem(name: String) {
   lazy val bin = try {
-    Right(Process("which foreman"))
+    val path = Process("which %s" format name).!!
+    if(path matches ".*%s\\s+".format(name)) Right(path)
+    else Left(new UnsupportedOperationException("%s gem not installed. try `gem install %s`" format(name, name)))
   } catch {
      case e => Left(e)
   }
   def onError(t: Throwable) = throw t
-  def start = bin.fold(onError, path
-    Process("%s start" format path)
-  }
-}*/
+  def call[T](cmd: String) = bin.fold(onError, { path =>
+    Process("%s %s" format(name, cmd))
+  })
+}
+
+object Heroku extends Gem("heroku") {
+  def logs = call("logs")
+  def ps = call("ps")
+  def create = call("heroku create --stack cedar")
+}
+
+object Foreman extends Gem("foreman") {
+  def start = call("start")
+}
 
 object Plugin extends sbt.Plugin {
   val Hero = config("hero") extend(Runtime)
 
   val prepare = TaskKey[Unit]("prepare", "Prepares project for heroku deployment")
-  val procfile = TaskKey[File]("profile", "Writes heroku procfile a projects base directory")
-  val main = TaskKey[Option[String]]("main", "Main class to run on Heroku")
+  val procfile = TaskKey[File]("profile", "Writes heroku Procfile to project base directory")
+  val main = TaskKey[Option[String]]("main", "Target Main class to run")
   val script = TaskKey[File]("script", "Generates driver script")
   val scriptName = SettingKey[String]("script-name", "Name of the generated driver script")
-  val pom = TaskKey[File]("pom", "generates and copies project pom to project base")
+  val pom = TaskKey[File]("pom", "Generates and copies project pom to project base")
 
-  //val foreman = TaskKey[Unit]("foreman", "...")
+  // heroku client api (not yet usable/until I figure out how to tail the process api :/)
+  val foreman = TaskKey[Unit]("foreman", "Start herko foreman env")
+  val logs = TaskKey[Unit]("logs", "Invokes Heroku client logs command")
+  val ps = TaskKey[Unit]("ps", "Invokes Heroku client ps command")
+  val create = TaskKey[Unit]("create", "Invokes Heroku client create command")
+  val push = TaskKey[Unit]("push", "Invokes Heroku client push command")
 
-  val IvyCached = """\S+/[.]ivy2/cache/(\S+.jar)""".r
+  private def foremanTask: Initialize[Task[Unit]] =
+    (streams) map {
+      (out) =>
+        (Foreman.start ! out.log)
+    }
+
+ private def logsTask: Initialize[Task[Unit]] =
+   (streams) map {
+     (out) =>
+      (Heroku.logs ! out.log)
+   }
+
+  private def psTask: Initialize[Task[Unit]] =
+   (streams) map {
+     (out) =>
+       (Heroku.ps ! out.log)
+   }
+
+  private def createTask: Initialize[Task[Unit]] =
+   (streams) map {
+     (out) => out.log.info("not yet implemented")
+   }
+
+  private def pushTask: Initialize[Task[Unit]] =
+    (streams) map {
+      (out) => out.log.info("not yet implemented")
+    }
 
   private def procfileTask: Initialize[Task[File]] =
     (baseDirectory, scriptName, streams) map {
@@ -85,11 +130,30 @@ object Plugin extends sbt.Plugin {
     }
 
   private def scriptTask: Initialize[Task[File]] =
-    (main, streams, scalaVersion, fullClasspath in Runtime, baseDirectory, moduleSettings, scriptName) map {
-      (main, out, sv, cp, base, mod, scriptName) => main match {
+    (main, streams, scalaVersion, /*fullClasspath in Runtime,*/ baseDirectory, moduleSettings, scriptName, update) map {
+      (main, out, sv, /*cp,*/ base, mod, scriptName, report) => main match {
         case Some(mainCls) =>
 
-          // hope for a moduleSetting that provides a moduleId
+          val onlyJar: ArtifactFilter = artifactFilter(`type` = "jar")
+          val onlyCompile: ConfigurationFilter = Set("compile")
+
+          (report select(onlyCompile)).foreach(println)
+
+          val IvyCachedParts = """(.*[.]ivy2/cache)/(\S+)/(\S+)/jars/(\S+)[.]jar""".r
+          val IvyCached = """(.*[.]ivy2/cache/\S+/\S+/jars/\S+[.]jar)""".r
+
+          def mvnize(ivy: String) = ivy match {
+             case IvyCachedParts(_, org, name, artifact) =>
+               "%s/%s/%s/%s.jar" format(
+                 org.replaceAll("[.]", java.io.File.separator),
+                 name,
+                 artifact.split("-").last,
+                 artifact
+             )
+             case notInIvy => error("not supported %s" format notInIvy)
+          }
+
+          // Hope for a moduleSetting that provides a moduleId
           // https://github.com/harrah/xsbt/blob/0.10/ivy/IvyConfigurations.scala#L51-73
           val mid = mod match {
             case InlineConfiguration(id, _, _, _, _, _, _) => id
@@ -100,23 +164,19 @@ object Plugin extends sbt.Plugin {
           // https://github.com/harrah/xsbt/blob/0.10/ivy/IvyInterface.scala#L12
           val (org, name, version) = (mid.organization, mid.name, mid.revision)
 
-          def mvnize(path: String) = path.replace("/jars", "").replace("[.]","/").replace("[.]","/")
-
-          val cpElems = cp.map(_.data.getPath).flatMap({
+          val cpElems = /*cp.map(_.data.getPath)*/(report select(onlyCompile)).map(_.getPath).flatMap({
                case IvyCached(jar) => Some(mvnize(jar))
-               case r => out.log.info("possibly leaving out cp resource %s" format r); None
+               case r => out.log.info("possibly leaving out cp resource %s." format r); None
             }) ++ Seq(
               "org/scala-lang/scala-library/%s/scala-library-%s.jar".format(sv, sv),
               "%s/%s_%s/%s/%s_%s-%s.jar".format(
                 org.replaceAll("[.]","/"),
                 name, sv, version, name, sv, version
-              ),
-              "target/scala-%s/classes".format(sv)
+              )
             )
 
           cpElems.foreach(e=>out.log.info("incl %s" format e))
-          val scriptBody =
-            Script(mainCls, cpElems)
+          val scriptBody = Script(mainCls, cpElems)
 
           out.log.info("Writing script/%s" format scriptName)
           val sf = new java.io.File(base, "script/%s" format scriptName)
@@ -127,14 +187,32 @@ object Plugin extends sbt.Plugin {
       }
     }
 
-  private def includingMvnPlugin(srcPath: String)(pom: xml.Node):xml.Node = {
+  private def includingMvnPlugin(srcPath: String, scalaVersion: String)(pom: xml.Node):xml.Node = {
     import scala.xml._
     import scala.xml.transform._
 
     def adopt(parent: Node, kid: Node) = parent match {
       case Elem(prefix, label, attrs, scope, kids @ _*) =>
         Elem(prefix, label, attrs, scope, kids ++ kid : _*)
-      case _ => error("Can only add children to elements!")
+      case _ => error("Only elements can adopt")
+    }
+
+    object EnsureEncoding extends RewriteRule {
+      override def transform(n: Node) = n match {
+        case proj@Elem(_, "project", _, _, _*) if((proj \ "properties") isEmpty) =>
+          adopt(
+            proj,
+            <properties>
+              <project.build.sourceEncoding>
+                UTF-8
+              </project.build.sourceEncoding>
+              <project.reporting.outputEncoding>
+                UTF-8
+              </project.reporting.outputEncoding>
+            </properties>
+          )
+        case node => node
+      }
     }
 
     object AddBuild extends RewriteRule {
@@ -149,6 +227,9 @@ object Plugin extends sbt.Plugin {
                    <groupId>org.scala-tools</groupId>
                    <artifactId>maven-scala-plugin</artifactId>
                    <version>2.14.1</version>
+                   <configuration>
+                    <scalaVersion>{scalaVersion}</scalaVersion>
+                   </configuration>
                   </plugin>
               </plugins>
             </build>
@@ -159,19 +240,24 @@ object Plugin extends sbt.Plugin {
 
     // todo: my test build was sparse
     // handling of other cases would make this
-    // much more robust
-    new RuleTransformer(AddBuild)(pom)
+    // much more robust :)
+    new RuleTransformer(AddBuild,EnsureEncoding)(pom)
   }
 
   val options: Seq[Setting[_]] = inConfig(Hero)(Seq(
-    (pomPostProcess in Global) <<= (pomPostProcess in Global, baseDirectory, sourceDirectory)(
-       (pp, base, src) => pp andThen(includingMvnPlugin(Path.relativeTo(base)(src).get))
+    (pomPostProcess in Global) <<= (pomPostProcess in Global, baseDirectory, sourceDirectory, scalaVersion)(
+       (pp, base, src, sv) => pp andThen(includingMvnPlugin(Path.relativeTo(base)(src).get, sv))
     ),
     main <<= (mainClass in Runtime).identity,
     scriptName := "hero",
     script <<= scriptTask,
     procfile <<= procfileTask,
     pom <<= pomTask,
-    prepare <<= Seq(script, procfile, pom).dependOn
+    prepare <<= Seq(script, procfile, pom).dependOn,
+    foreman <<= foremanTask,
+    logs <<= logsTask,
+    ps <<= psTask,
+    create <<= createTask,
+    push <<= pushTask
   ))
 }
