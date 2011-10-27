6f4916e9070d7dfbb59c936f27bbbca0299d07cc
softprops <d.tangren@gmail.com>
2011-10-26 20:43:18 -0400

    use sbts jline abstraction for added backspace support (masking patch should be in an upcoming release)

--- src/main/scala/prompt.scala
+++ src/main/scala/prompt.scala
@@ -1,10 +1,12 @@
 package heroic
 
+// todo factor this into its own plugin
 object Prompt {
+  import jline._
   val Okays = Seq("y","yes", "yep", "yea")
   val Nos = Seq("n", "no", "nope", "nah")
-  def ask[T](txt: String)(f: String => T): T = {
-    print(txt)
-    f(readLine)
-  }
+  def askDiscretely[T](txt: String)(f: String => T): T =
+    f(sbt.SimpleReader.readLine(txt).get)
+  def ask[T](txt: String)(f: String => T): T =
+    f(sbt.SimpleReader.readLine(txt).get)
 }
