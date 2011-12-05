d09078995600250c94ff5367e030db8e94f19c78
softprops <d.tangren@gmail.com>
2011-12-04 20:09:49 -0500

    add descrete input mask

--- src/main/scala/prompt.scala
+++ src/main/scala/prompt.scala
@@ -6,7 +6,7 @@ object Prompt {
   val Okays = Seq("y","yes", "yep", "yea")
   val Nos = Seq("n", "no", "nope", "nah")
   def askDiscretely[T](txt: String)(f: String => T): T =
-    f(sbt.SimpleReader.readLine(txt).get)
+    f(sbt.SimpleReader.readLine(txt, Some('*')).get)
   def ask[T](txt: String)(f: String => T): T =
     f(sbt.SimpleReader.readLine(txt).get)
 }
